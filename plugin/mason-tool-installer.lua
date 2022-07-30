b2d311b02cbc1924f9e59bdad86b2ba0a16363e4
monster0506 <91501317+Monster0506@users.noreply.github.com>
2022-07-29 20:33:45 -0400

    R  lua/mason-tool-installer/init.lua -> lua/mason-installer/init.lua M  plugin/mason-tool-installer.lua

--- plugin/mason-tool-installer.lua
+++ plugin/mason-tool-installer.lua
@@ -1,7 +1,14 @@
-vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-  callback = require('mason-tool-installer').run_on_start,
-})
+vim.api.nvim_create_autocmd(
+    {"VimEnter"},
+    {
+        callback = require("mason-installer").run_on_start
+    }
+)
 
-vim.api.nvim_create_user_command('MasonToolsUpdate', function()
-  require('mason-tool-installer').check_install(true)
-end, { force = true })
+vim.api.nvim_create_user_command(
+    "MasonToolsUpdate",
+    function()
+        require("mason-installer").check_install(true)
+    end,
+    {force = true}
+)
